cae8c497ac458b0ccb6ea751b296df2ae8cdfdf5
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-02-12 21:34:43 -0500

    Changed project to make use of Spring-boot

--- src/main/java/standalone/Main.java
+++ src/main/java/standalone/Main.java
@@ -1,11 +1,14 @@
 package standalone;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+@Configuration
+@ImportResource("classpath:httpgateway.xml")
 public class Main {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/httpgateway.xml");
-		applicationContext.registerShutdownHook();
+		SpringApplication.run(Main.class, args);
 	}
 }
