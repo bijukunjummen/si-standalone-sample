4083deec590c94217698d2256cdacb06a6c48677
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-11-17 09:42:50 -0800

    Upgraded to use Spring Boot 1.4.2.RELEASE, fixed USGS related url

--- src/test/java/standalone/TestHttpOutboundGateway.java
+++ src/test/java/standalone/TestHttpOutboundGateway.java
@@ -1,29 +1,28 @@
 package standalone;
 
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.Message;
-import org.springframework.integration.core.PollableChannel;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-httpgateway.xml")
 public class TestHttpOutboundGateway {
 	
-	@Autowired @Qualifier("test.channel") PollableChannel testChannel;
+	@Autowired @Qualifier("test.channel")
+	PollableChannel testChannel;
 
 	@Test
 	public void testHttpOutbound() {
 		Message<?> message = testChannel.receive();
-		assertThat(message.getPayload(), is(notNullValue()));
+		assertThat(message.getPayload()).isNotNull();
 	}
 	
 
